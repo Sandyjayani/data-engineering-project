efdd2cdb8bb25fd6c3b4a0a328d65c12dc5bf82c
dkettchen <dkettchen@gmail.com>
2024-08-14 14:59:24 +0100

    prep lambda handler files with imports and empty functions

---
+++ src/lambda_handler.py
@@ -0,0 +1,6 @@
+from util_functions.get_db_connection import create_connection
+from util_functions.get_table import get_table
+from util_functions.upload_to_s3_util_func import upload_tables_to_s3
+
+def lambda_handler(event, context):
+    pass
---
+++ test/test_lambda_handler.py
@@ -0,0 +1,4 @@
+from src.lambda_handler import lambda_handler
+
+def test_():
+    pass
