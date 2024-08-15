2a17746e30350b25924d9e24662819a1e301fcef
Martin Colbourne <martinlambc@gmail.com>
2024-08-15 11:47:33 +0100

    updates from merge from main

--- test/test_get_db_connection.py
+++ test/test_get_db_connection.py
@@ -3,13 +3,12 @@ import os
 import boto3
 import pytest
 from moto import mock_aws
-from unittest.mock import patch, MagicMock 
+from unittest.mock import patch, MagicMock
 import json
 from botocore.exceptions import ClientError
 
 
-    
-@pytest.fixture(scope='function')
+@pytest.fixture(scope="function")
 def aws_creds():
     os.environ["AWS_ACCESS_KEY_ID"] = "test"
     os.environ["AWS_SECRET_ACCESS_KEY"] = "test"
@@ -18,64 +17,71 @@ def aws_creds():
     os.environ["AWS_DEAFULT_REGION"] = "eu-west-2"
 
 
-@pytest.fixture(scope='function')
+@pytest.fixture(scope="function")
 def secrets_client(aws_creds):
     with mock_aws():
-        sectrets_client = boto3.client('secretsmanager')
+        sectrets_client = boto3.client("secretsmanager")
         yield sectrets_client
 
 
-@patch('boto3.session.Session')
+@patch("boto3.session.Session")
 def test_get_secret_configuration(mock_session):
     # Configure the mock session
     mock_session_instance = MagicMock()
     mock_session.return_value = mock_session_instance
-    
+
     # Configure the mock client
     mock_client = MagicMock()
     mock_session_instance.client.return_value = mock_client
-    
+
     # Set up specific return values or behaviors
     mock_client.get_secret_value.return_value = {
-        'SecretString': '{"key": "mocked_value"}'
+        "SecretString": '{"key": "mocked_value"}'
     }
 
     result = get_secret()
 
     mock_session.assert_called_once()
-    
-    assert json.loads(result) == {"key": "mocked_value"}
-     
 
-@patch('boto3.session.Session')
+    assert json.loads(result) == {"key": "mocked_value"}
+
+
+@patch("boto3.session.Session")
 def test_get_secret_exception(mock_session):
-        # Configure the mock session
+    # Configure the mock session
     mock_session_instance = MagicMock()
     mock_session.return_value = mock_session_instance
-    
+
     # Configure the mock client
     mock_client = MagicMock()
     mock_session_instance.client.return_value = mock_client
 
     mock_client.get_secret_value.side_effect = ClientError(
-            {'Error': {'Code': 'ResourceNotFoundException', 'Message': 'Secret not found'}},
-            'GetSecretValue'
-        )
-   
-    with pytest.raises(ClientError) as excinfo:
-        mock_client.get_secret_value(SecretId='nonexistent-secret')
-        
-    assert 'ResourceNotFoundException' in str(excinfo.value)
-    
-@patch('test_get_db_connection.create_connection')
-@patch('src.get_db_connection.get_secret')
-def test_create_connection(mock_get_secrets,mock_create_connection):
-   
-    mock_get_secrets.return_value = json.dumps('{"user": "test_user","password":"test_password","host":"test_host","database":"test_database","port":5432}')
-    
+        {"Error": {"Code": "ResourceNotFoundException", "Message": "Secret not found"}},
+        "GetSecretValue",
+    )
 
+    with pytest.raises(ClientError) as excinfo:
+        mock_client.get_secret_value(SecretId="nonexistent-secret")
+
+    assert "ResourceNotFoundException" in str(excinfo.value)
+
+
+@patch("test_get_db_connection.create_connection")
+@patch("src.get_db_connection.get_secret")
+def test_create_connection(mock_get_secrets, mock_create_connection):
+    mock_get_secrets.return_value = json.dumps(
+        '{"user": "test_user","password":"test_password","host":"test_host","database":"test_database","port":5432}'
+    )
     result = create_connection()
     print(result)
-
     # assert mock_create_connection.call_count == 1
-    mock_create_connection.assert_called_with({"user": "test_user","password":"test_password","host":"test_host","database":"test_database","port":5432})
+    mock_create_connection.assert_called_with(
+        {
+            "user": "test_user",
+            "password": "test_password",
+            "host": "test_host",
+            "database": "test_database",
+            "port": 5432,
+        }
+    )
--- test/test_upload_to_s3_util_func.py
+++ test/test_upload_to_s3_util_func.py
@@ -1,5 +1,5 @@
 import pytest
-from upload_to_s3_util_func import upload_tables_to_s3, save_timestamps
+from src.upload_to_s3_util_func import upload_tables_to_s3, save_timestamps
 from moto import mock_aws
 import boto3
 from unittest.mock import patch, Mock
@@ -27,103 +27,102 @@ def mock_client(aws_credentials):
 
 class TestUploadToS3:
     @pytest.mark.it("Test if the correct confirmation message is returned")
-    @patch('upload_to_s3_util_func.datetime')
-    def test_confirmation_message_is_returned(
-            self, mock_datetime, mock_client):
+    @patch("upload_to_s3_util_func.datetime")
+    def test_confirmation_message_is_returned(self, mock_datetime, mock_client):
 
-        test_table = 'test_table'
-        test_bucket = 'test_bucket'
-        test_key = 'test_table/2024/8/13/16-57/test_table-2024-08-13_16-57.csv'
+        test_table = "test_table"
+        test_bucket = "test_bucket"
+        test_key = "test_table/2024/8/13/16-57/test_table-2024-08-13_16-57.csv"
 
         mock_client.create_bucket(
-                Bucket=test_bucket,
-                CreateBucketConfiguration={"LocationConstraint": "eu-west-2"})
+            Bucket=test_bucket,
+            CreateBucketConfiguration={"LocationConstraint": "eu-west-2"},
+        )
 
         mock_now = datetime(2024, 8, 13, 16, 57)
         mock_datetime.now.return_value = mock_now
 
         mock_df = Mock(spec=pd.DataFrame)
-        mock_df.to_csv.return_value = 'mock_csv'
+        mock_df.to_csv.return_value = "mock_csv"
 
         expected = (
-                    f"Table {test_table} has been uploaded to {test_bucket} "
-                    f"with key {test_key}."
-                    )
-        assert upload_tables_to_s3(
-                    mock_df, test_table, test_bucket
-                ) == expected
+            f"Table {test_table} has been uploaded to {test_bucket} "
+            f"with key {test_key}."
+        )
+        assert upload_tables_to_s3(mock_df, test_table, test_bucket) == expected
 
     @pytest.mark.it("Test if correct file being uploaded to the given bucket")
-    @patch('upload_to_s3_util_func.datetime')
+    @patch("upload_to_s3_util_func.datetime")
     def test_correct_files_being_uploaded(self, mock_datetime, mock_client):
-        test_table = 'test_table'
-        test_bucket = 'test_bucket'
-        test_key = 'test_table/2024/8/13/16-57/test_table-2024-08-13_16-57.csv'
+        test_table = "test_table"
+        test_bucket = "test_bucket"
+        test_key = "test_table/2024/8/13/16-57/test_table-2024-08-13_16-57.csv"
 
         mock_client.create_bucket(
-                Bucket=test_bucket,
-                CreateBucketConfiguration={"LocationConstraint": "eu-west-2"})
+            Bucket=test_bucket,
+            CreateBucketConfiguration={"LocationConstraint": "eu-west-2"},
+        )
 
         mock_now = datetime(2024, 8, 13, 16, 57)
         mock_datetime.now.return_value = mock_now
 
         mock_df = Mock(spec=pd.DataFrame)
-        mock_df.to_csv.return_value = 'mock_csv'
+        mock_df.to_csv.return_value = "mock_csv"
 
         upload_tables_to_s3(mock_df, test_table, test_bucket)
 
         response = mock_client.list_objects_v2(Bucket=test_bucket)
 
-        assert response['KeyCount'] == 2
-        assert response['Contents'][0]['Key'] == test_key
+        assert response["KeyCount"] == 2
+        assert response["Contents"][0]["Key"] == test_key
 
         mock_now = datetime(2024, 8, 13, 17, 27)
         mock_datetime.now.return_value = mock_now
 
-        test_key2 = (
-                'test_table/2024/8/13/17-27/test_table-2024-08-13_17-27.csv'
-                    )
+        test_key2 = "test_table/2024/8/13/17-27/test_table-2024-08-13_17-27.csv"
 
         upload_tables_to_s3(mock_df, test_table, test_bucket)
 
         response2 = mock_client.list_objects_v2(Bucket=test_bucket)
-        assert response2['KeyCount'] == 3
-        assert response2['Contents'][1]['Key'] == test_key2
+        assert response2["KeyCount"] == 3
+        assert response2["Contents"][1]["Key"] == test_key2
 
 
 class TestSaveTimestamps:
-    @pytest.mark.it('Test new_timestamp is created when no timestamp table')
+    @pytest.mark.it("Test new_timestamp is created when no timestamp table")
     def test_new_timestamp(self, mock_client):
-        test_table = 'test_table_timestamp'
-        test_bucket = 'test_bucket_timestamp'
-        test_timestamp = '2024-08-13_17-27'
+        test_table = "test_table_timestamp"
+        test_bucket = "test_bucket_timestamp"
+        test_timestamp = "2024-08-13_17-27"
 
         mock_client.create_bucket(
             Bucket=test_bucket,
-            CreateBucketConfiguration={"LocationConstraint": "eu-west-2"})
+            CreateBucketConfiguration={"LocationConstraint": "eu-west-2"},
+        )
 
         save_timestamps(test_table, test_timestamp, test_bucket)
 
         response = mock_client.get_object(
-            Bucket=test_bucket,
-            Key=f"{test_table}/timestamps.csv")
-        content = response['Body'].read().decode('utf-8')
-        expected_df = pd.DataFrame({'Date': [test_timestamp]})
+            Bucket=test_bucket, Key=f"{test_table}/timestamps.csv"
+        )
+        content = response["Body"].read().decode("utf-8")
+        expected_df = pd.DataFrame({"Date": [test_timestamp]})
         expected_csv = expected_df.to_csv(index=False)
         assert content == expected_csv
 
-    @pytest.mark.it('Test if it appends to an existing timestamp table')
+    @pytest.mark.it("Test if it appends to an existing timestamp table")
     def test_append_timestamp(self, mock_client):
-        test_table = 'test_table_timestamp'
-        test_bucket = 'test_bucket_timestamp'
-        test_initial_timestamp = '2024-08-13_17-27'
-        test_new_timestamp = '2024-08-13_17-57'
+        test_table = "test_table_timestamp"
+        test_bucket = "test_bucket_timestamp"
+        test_initial_timestamp = "2024-08-13_17-27"
+        test_new_timestamp = "2024-08-13_17-57"
 
         mock_client.create_bucket(
             Bucket=test_bucket,
-            CreateBucketConfiguration={"LocationConstraint": "eu-west-2"})
+            CreateBucketConfiguration={"LocationConstraint": "eu-west-2"},
+        )
 
-        initial_df = pd.DataFrame({'Date': [test_initial_timestamp]})
+        initial_df = pd.DataFrame({"Date": [test_initial_timestamp]})
 
         initial_csv_buffer = StringIO()
         initial_df.to_csv(initial_csv_buffer, index=False)
@@ -131,7 +130,7 @@ class TestSaveTimestamps:
 
         save_timestamps(test_table, test_initial_timestamp, test_bucket)
 
-        new_df = pd.DataFrame({'Date': [test_new_timestamp]})
+        new_df = pd.DataFrame({"Date": [test_new_timestamp]})
         new_csv_buffer = StringIO()
         new_df.to_csv(new_csv_buffer, index=False)
         new_csv_buffer.seek(0)
@@ -139,11 +138,11 @@ class TestSaveTimestamps:
         save_timestamps(test_table, test_new_timestamp, test_bucket)
 
         response = mock_client.get_object(
-                    Bucket=test_bucket,
-                    Key=f"{test_table}/timestamps.csv")
-        content = response['Body'].read().decode('utf-8')
+            Bucket=test_bucket, Key=f"{test_table}/timestamps.csv"
+        )
+        content = response["Body"].read().decode("utf-8")
         expected_df = pd.DataFrame(
-            {'Date': [test_initial_timestamp, test_new_timestamp]}
-            )
+            {"Date": [test_initial_timestamp, test_new_timestamp]}
+        )
         expected_csv = expected_df.to_csv(index=False)
         assert content == expected_csv
