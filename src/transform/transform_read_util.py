78009a31193d2ddaa925abb55a8b6caac97e928c
sandyjayani <sandeepjayani@gmail.com>
2024-08-19 16:54:27 +0100

    Implement S3 Data ingestion function

--- src/transform/transform_read_util.py
+++ src/transform/transform_read_util.py
@@ -2,11 +2,24 @@ import boto3
 import pandas as pd
 from src.extraction.setup_logger import setup_logger
 from src.extraction.get_timestamp import get_timestamp
-from datetime import datetime
 from io import StringIO
 
 
 def ingestion_data_from_s3():
+    """
+    This function reads latest file from s3 ingestion bucket and returns dict of DataFrames.
+    This function connects to the "smith-morra-ingestion-bucket" s3 bucket and retrieves the latest csv files from each table and reads the CSV files into pandas DataFrames. the DataFrames are stored in a dictionary with the table names as keys.
+
+    Returns:
+        Dict: key-table_name, value-df
+
+    Raises:
+        Exception: if there is an error loading data from s3, an exception is raised.
+
+    Logging:
+        Logs at the start and successful completion for each table. logs error if data loading fails.
+    """
+
 
     logger = setup_logger("read_from_ingestion_s3_bucket_logger")
     BUCKET_NAME = "smith-morra-ingestion-bucket"
@@ -23,6 +36,7 @@ def ingestion_data_from_s3():
         "address",
         "counterparty",
     ]
+
 
     s3 = boto3.client('s3')
     data_dicts = {}
@@ -41,7 +55,7 @@ def ingestion_data_from_s3():
                 f"{timestamp_datetime.hour}-{timestamp_datetime.minute}/"
                 f"{table}-{timestamp_str}.csv"
             )
-
+            print(s3_key)
             obj = s3.get_object(Bucket=BUCKET_NAME, Key=s3_key)['Body']
 
             df = pd.read_csv(StringIO(obj.read().decode('utf-8')))
