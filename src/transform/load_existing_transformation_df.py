f673e804198132f04733397e0640f105f93f923d
EricaC2401 <cwc3214@gmail.com>
2024-08-20 14:17:29 +0100

    finished the bad path test

--- src/transform/load_existing_transformation_df.py
+++ src/transform/load_existing_transformation_df.py
@@ -3,6 +3,9 @@ from datetime import datetime
 import pandas as pd
 from io import BytesIO
 from re import search
+from src.extraction.setup_logger import setup_logger
+
+logger = setup_logger("Load and Combine files logger")
 
 '''
     - It would be a function that load all the Parquet files in the folder (e.g. dim_staff) 
@@ -10,6 +13,9 @@ from re import search
     - It could be used in the functions for dim_staff and dim_counterparty and also the loading stage
 '''
 def extract_timestamp(filename):
+    '''
+    extract the timestamp from the fileanme and return the it in datetime 
+    '''
     match = search(r'(\d{4}-\d{2}-\d{2})_(\d{2})\.(\d{2})\.(\d{2})\.parquet$', filename)
     if match:
         date_str = match.group(1)
@@ -24,12 +30,12 @@ def extract_timestamp(filename):
 def load_and_combine_transformed_tables(table_name: str) -> pd.DataFrame:
     '''
     - create a s3 client 
-    - create an empty list
+    - create an empty dict
     - create a variable storing a list of keys in the table_name folder
     - loop over the list
     -   check if the file endswith 'parquet'
     -   if so get the object from s3 and load as df
-    -   append the df to the list
+    -   append the df to the dict
     - sort the df by the timestamp
     - concate all df in the list into a single df 
     - return the df 
@@ -45,14 +51,24 @@ def load_and_combine_transformed_tables(table_name: str) -> pd.DataFrame:
     for content in s3_response.get('Contents', []):
         key = content.get('Key','')
         if key.endswith('.parquet'):
-            response = s3_client.get_object(Bucket=BUCKET_NAME, Key=key)
-            df = pd.read_parquet(BytesIO(response['Body'].read()))
-            df_dict[key] = df
+            logger.info(f"Processing file: {key}")
+            try:
+                response = s3_client.get_object(Bucket=BUCKET_NAME, Key=key)
+                df = pd.read_parquet(BytesIO(response['Body'].read()))
+                df_dict[key] = df
+                logger.info(f"Loaded DataFrame from: {key}")
+            except Exception as e:
+                logger.error(f"Failed to load DataFrame from {key}. Error: {e}")
+                raise e
             
     if df_dict:
         df_list = sorted(df_dict, key=extract_timestamp)
         sorted_df_list = [df_dict[key] for key in df_list]
         combined_df = pd.concat(sorted_df_list, ignore_index=True)
+        logger.info(f"Combined tables in {table_name} into a single DataFrame.")
         return combined_df
     else:
-        return pd.DataFrame
+        logger.warning(f"No objects found in the bucket with folder name: {table_name}")
+        return pd.DataFrame()
+        
+        
