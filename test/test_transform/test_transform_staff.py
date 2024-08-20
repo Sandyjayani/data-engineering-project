3323c903263bd92a7cf0754725b6144af5981b3d
Martin Colbourne <martinlambc@gmail.com>
2024-08-20 14:24:56 +0100

    add transform_staff tests

--- test/test_transform/test_transform_staff.py
+++ test/test_transform/test_transform_staff.py
@@ -1,30 +1,34 @@
 import pandas as pd
+from unittest.mock import Mock, patch
 import pytest
 from src.transform.transform_staff import transform_staff as ts
 
 
 @pytest.fixture
-def test_df():
-    test_df = pd.read_csv('test/test_transform/test_staff.csv')
-    return test_df
+def test_df_dict():
+    return {'staff': pd.read_csv('test/test_transform/test_staff.csv'),
+            'department': pd.read_csv('test/test_transform/test_dep.csv')}
+     
 
-
-@pytest.mark.it('Returns dataframe')
-def test_returns_df(test_df):
-    output = ts(test_df)
+@pytest.mark.it('Returns dataframe if passed staff data')
+@patch('src.transform.load_existing_transformation_df')
+def test_returns_df(mock_load_existing_transformation_df, test_df_dict):
+    output = ts(test_df_dict)
     assert isinstance(output, pd.DataFrame)
 
 
 @pytest.mark.it('Returns dataframe with the correct columns')
-def test_returns_df_with_correct_cols(test_df):
-        new_df = ts(test_df)
+@patch('src.transform.load_existing_transformation_df')
+def test_returns_df_with_correct_cols(mock_load_existing_transformation_df, test_df_dict):
+        new_df = ts(test_df_dict)
         expected_cols = ["staff_id", "first_name", "last_name", "department_name", "location", "email_address"]
         output_cols = list(new_df.columns)
         assert expected_cols == output_cols
 
 
 @pytest.mark.it('Returns new dataframe')
-def test_returns_new_df(test_df):
-    orig_df = test_df
-    output_df = ts(test_df)
+@patch('src.transform.load_existing_transformation_df')
+def test_returns_new_df(mock_load_existing_transformation_df, test_df_dict):
+    orig_df = test_df_dict
+    output_df = ts(test_df_dict)
     assert output_df is not orig_df
