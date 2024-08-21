5f1e87869cfa5557f20a6b5eb60daaaa3d803460
dkettchen <dkettchen@gmail.com>
2024-08-21 09:59:20 +0100

    create load folder and check if there's stuff in database

---
+++ src/load/get_db_connection.py
@@ -0,0 +1,33 @@
+import boto3
+from botocore.exceptions import ClientError
+from pg8000.native import Connection, Error
+import pandas as pd
+import json
+import os
+
+if os.environ.get("AWS_EXECUTION_ENV") is not None:
+    from get_secret import get_secret
+else:
+    from src.load.get_secret import get_secret
+
+
+def create_connection(stage_name):
+    # Retrieve the secret
+    if stage_name == "extraction":
+        secret = get_secret("DataSource_PostgresDB_Credentials")
+    elif stage_name == "load":
+        secret = get_secret("DataTarget_PostgresDB_Credentials")
+    secret_dict = json.loads(secret)
+
+    # Extract credentials from the secret
+    user = secret_dict["user"]
+    password = secret_dict["password"]
+    host = secret_dict["host"]
+    database = secret_dict["database"]
+    port = secret_dict["port"]
+
+    # Establish the database connection
+    conn = Connection(
+        user=user, database=database, host=host, password=password, port=port
+    )
+    return conn
