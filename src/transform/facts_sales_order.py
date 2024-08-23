b88a873e32e80794b66570b20ff14fdba61a7067
EricaC2401 <cwc3214@gmail.com>
2024-08-23 10:14:32 +0100

    refactor facts_sales_order.py

--- src/transform/facts_sales_order.py
+++ src/transform/facts_sales_order.py
@@ -1,6 +1,6 @@
 import pandas as pd
 from copy import deepcopy
-from datetime import date, time, datetime
+from datetime import datetime
 import os
 
 if os.environ.get("AWS_EXECUTION_ENV"):
@@ -8,7 +8,7 @@ if os.environ.get("AWS_EXECUTION_ENV"):
 else:
     from src.transform.setup_logger import setup_logger  
 
-def transform_sales_order(dataframe):
+def transform_sales_order(raw_dataframe):
     """
     takes a sales_order OLTP dataframe
 
@@ -20,79 +20,47 @@ def transform_sales_order(dataframe):
     logger = setup_logger('transform_staff')
     logger.info('Starting transform_sales_order')
 
-    if list(dataframe.columns) != [
-        "sales_order_id",
-        "created_at",
-        "last_updated",
-        "design_id",
-        "staff_id",
-        "counterparty_id",
-        "units_sold",
-        "unit_price",
-        "currency_id",
-        "agreed_delivery_date",
-        "agreed_payment_date",
-        "agreed_delivery_location_id",
-    ]:
-        raise ValueError
+    expected_columns = [
+                        "sales_order_id",
+                        "created_at",
+                        "last_updated",
+                        "design_id",
+                        "staff_id",
+                        "counterparty_id",
+                        "units_sold",
+                        "unit_price",
+                        "currency_id",
+                        "agreed_delivery_date",
+                        "agreed_payment_date",
+                        "agreed_delivery_location_id",
+                    ]
+        
+    if list(raw_dataframe.columns) != expected_columns:
+        raise ValueError('Dataframe columns do not match the expected schema')
 
-    facts_columns = [
-        "sales_order_id",
-        "created_date",
-        "created_time",
-        "last_updated_date",
-        "last_updated_time",
-        "sales_staff_id",
-        "counterparty_id",
-        "units_sold",
-        "unit_price",
-        "currency_id",
-        "design_id",
-        "agreed_payment_date",
-        "agreed_delivery_date",
-        "agreed_delivery_location_id",
-    ]
+    dataframe = deepcopy(raw_dataframe)
 
-    all_rows = []
-    for index in range(len(dataframe)):
-        row = dataframe.loc[index]
+    dataframe['created_at'] = pd.to_datetime(dataframe['created_at'])
+    dataframe['last_updated'] = pd.to_datetime(dataframe['last_updated'])
+    dataframe['agreed_payment_date'] = pd.to_datetime(dataframe['agreed_payment_date']).dt.date
+    dataframe['agreed_delivery_date'] = pd.to_datetime(dataframe['agreed_delivery_date']).dt.date
 
-        order_id = row.iloc[0]
-        created_date = datetime.strptime(row.iloc[1], "%Y-%m-%d %H:%M:%S.%f").date()
-        created_time = datetime.strptime(row.iloc[1], "%Y-%m-%d %H:%M:%S.%f").time()
-        last_updated_date = datetime.strptime(row.iloc[2], "%Y-%m-%d %H:%M:%S.%f").date()
-        last_updated_time = datetime.strptime(row.iloc[2], "%Y-%m-%d %H:%M:%S.%f").time()
-        sales_staff_id = row.iloc[4]
-        counterparty_id = row.iloc[5]
-        units_sold = row.iloc[6]
-        unit_price = row.iloc[7]
-        currency_id = row.iloc[8]
-        design_id = row.iloc[3]
-        agreed_payment_date = datetime.strptime(row.iloc[10], "%Y-%m-%d").date()
-        agreed_delivery_date = datetime.strptime(row.iloc[9], "%Y-%m-%d").date()
-        agreed_delivery_location_id = row.iloc[11]
+    new_df = pd.DataFrame({
+        "sales_order_id": dataframe["sales_order_id"],
+        "created_date": dataframe['created_at'].dt.date,
+        "created_time": dataframe['created_at'].dt.time,
+        "last_updated_date": dataframe['last_updated'].dt.date,
+        "last_updated_time": dataframe['last_updated'].dt.time,
+        "sales_staff_id": dataframe["staff_id"],
+        "counterparty_id": dataframe["counterparty_id"],
+        "units_sold": dataframe["units_sold"],
+        "unit_price": dataframe["unit_price"],
+        "currency_id": dataframe["currency_id"],
+        "design_id": dataframe["design_id"],
+        "agreed_payment_date": dataframe["agreed_payment_date"],
+        "agreed_delivery_date": dataframe["agreed_delivery_date"],
+        "agreed_delivery_location_id": dataframe["agreed_delivery_location_id"]
+    })
 
-        new_row = [
-            order_id,
-            created_date,
-            created_time,
-            last_updated_date,
-            last_updated_time,
-            sales_staff_id,
-            counterparty_id,
-            units_sold,
-            unit_price,
-            currency_id,
-            design_id,
-            agreed_payment_date,
-            agreed_delivery_date,
-            agreed_delivery_location_id,
-        ]
-
-        new_series = pd.Series(data=new_row, index=facts_columns)
-
-        all_rows.append(new_series)
-
-    new_df = pd.DataFrame(data=all_rows, columns=facts_columns)
     logger.info('Transform_sales_order completed')
     return new_df
