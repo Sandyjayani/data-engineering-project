b1d484ebef70541dccdd3d1b52b137cfc5026529
Martin Colbourne <martinlambc@gmail.com>
2024-08-25 21:21:49 +0100

    update extraction with dynamic response

--- src/extraction/extraction.py
+++ src/extraction/extraction.py
@@ -1,8 +1,6 @@
-import json
-import sys
 import os
 
-if os.environ.get("AWS_EXECUTION_ENV") is not None:
+if os.environ.get("AWS_EXECUTION_ENV"):
     from get_db_connection import create_connection
     from get_table import get_table
     from upload_to_s3_util_func import upload_tables_to_s3
@@ -58,32 +56,35 @@ def lambda_handler(event, context):
             "counterparty",
         ]
 
+        new_data_ingested = False
+
         for table_name in sorted(TABLE_NAMES):
 
             logger.info(f"Getting last timestamp for {table_name} table.")
             last_timestamp = get_timestamp(table_name)
-            logger.info(
-                f"Last timestamp for {table_name} table ({last_timestamp}) has been retrieved"
-            )
+            logger.info(f"Last timestamp for {table_name} table retrieved")
 
-            logger.info(f"Getting newest table data from {table_name} table.")
+            logger.info(f"Getting newest data from {table_name} table.")
             table_data = get_table(table_name, conn, last_timestamp)
-            logger.info(
-                f"Successfully retrieved new table data from {table_name} table."
-            )
+            if table_data is not None:
+                new_data_ingested = True
+            logger.info(f"New data extracted from {table_name} table.")
 
             logger.info(f"Uploading newest {table_name} data to s3 bucket.")
-            upload_response = upload_tables_to_s3(table_data, table_name, BUCKET_NAME)
-            logger.info(f"Successfully uploaded newest {table_name} data to s3 bucket.")
+            upload_tables_to_s3(table_data, table_name, BUCKET_NAME)
+            logger.info(f"New data from {table_name} upload to s3 bucket.")
 
         logger.info("Table iteration has finished.")
 
         logger.info("Creating output response object.")
+        output_text = "Extraction lambda executed successfully: "
 
-        return {
-            "statusCode": 200,
-            "body": json.dumps("Extraction Lambda is executed successfully"),
-        }
+        if new_data_ingested:
+            output_text += "new data ingested"
+        else:
+            output_text += "no new data ingested"
+
+        return {"statusCode": 200, "body": output_text}
     except Exception as e:
         logger.critical(f"Critical error: {e}")
         raise e
