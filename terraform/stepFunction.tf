5d5ce26afd14b9034479a73b99cd8ba46d5127e6
EricaC2401 <cwc3214@gmail.com>
2024-08-27 10:40:09 +0100

    add the scheduler for the step function and refactor the placeholder of step function

--- terraform/stepFunction.tf
+++ terraform/stepFunction.tf
@@ -1,31 +1,87 @@
 resource "aws_sfn_state_machine" "unified_state_machine" {
-  name = "unified-state-machine"
+  name     = "unified-state-machine"
   role_arn = aws_iam_role.unified_state_machine_role.arn
   definition = <<EOF
   {
-  "Comment": "An unifiied state machine for the entire ETL process",
-  "StartAt" : "Extract",
-  "States": {
-    "Extract":{
+    "Comment": "A unified state machine for the entire ETL process",
+    "StartAt": "Extract",
+    "States": {
+      "Extract": {
         "Type": "Task",
         "Resource": "arn:aws:states:::lambda:invoke",
         "OutputPath": "$.Payload",
         "Parameters": {
-            "Payload.$": "$",
-            "FunctionName": "${module.extraction.lambda_function_extraction_arn}"
+          "Payload.$": "$",
+          "FunctionName": "${module.extraction.lambda_function_extraction_arn}"
+        },
+        "Retry": [
+          {
+            "ErrorEquals": [
+              "States.ALL"
+            ],
+            "MaxAttempts": 3,
+            "BackoffRate": 1.5,
+            "IntervalSeconds": 1
+          }
+        ],
+        "Catch": [
+          {
+            "ErrorEquals": [
+              "States.ALL"
+            ],
+            "Next": "Pass"
+          }
+        ],
+        "Next": "New Data Choice",
+        "OutputPath": "$.Payload"
       },
-      "Retry": [
-        {
-          "ErrorEquals": [
-            "States.ALL"
-          ],
-          "MaxAttempts": 3,
-          "BackoffRate": 1.5,
-          "IntervalSeconds": 1
-        }
-      ],
-      "End": true
-    }
+      "New Data Choice": {
+        "Type": "Choice",
+        "Choices": [
+          {
+            "Variable": "$.body",
+            "StringMatches": "*no new data ingested",
+            "Next": "Pass"
+          }
+        ],
+        "Default": "Transform"
+      },
+      "Pass": {
+        "Type": "Pass",
+        "End": true
+      },
+      "Transform": {
+        "Type": "Task",
+        "Resource": "arn:aws:states:::lambda:invoke",
+        "OutputPath": "$.Payload",
+        "Parameters": {
+          "Payload.$": "$",
+          "FunctionName": "${module.transformation.lambda_function_transformation_arn}"
+        },
+        "Retry": [
+          {
+            "ErrorEquals": [
+              "Lambda.ServiceException",
+              "Lambda.AWSLambdaException",
+              "Lambda.SdkClientException",
+              "Lambda.TooManyRequestsException"
+            ],
+            "IntervalSeconds": 1,
+            "MaxAttempts": 3,
+            "BackoffRate": 2
+          }
+        ],
+        "Catch": [
+          {
+            "ErrorEquals": [
+              "States.ALL"
+            ],
+            "ResultPath": null,
+            "Next": "Pass"
+          }
+        ],
+        "End": true
+      }
     }
   }
   EOF
--- terraform/stepFunction_scheduler.tf
+++ terraform/stepFunction_scheduler.tf
@@ -1,19 +1,17 @@
 # placeholder setup a scheduler for step function 
 
-# resource "aws_cloudwatch_event_rule" "scheduler" {
+resource "aws_cloudwatch_event_rule" "scheduler" {
+  name = "trigger-step-function"
+  description = "trigger step function every 15 minutes"
+  schedule_expression = "rate(15 minutes)"
+}
 
-#   name = "trigger-step-function"
-#   description = "trigger step function every 3 minutes"
-#   schedule_expression = "rate(3 minutes)"
-# }
+resource "aws_cloudwatch_event_target" "step_function_target" {
+  rule      = aws_cloudwatch_event_rule.scheduler.name
+  arn       = aws_sfn_state_machine.unified_state_machine.arn
+  role_arn  = aws_iam_role.unified_state_machine_role.arn
+}
 
-# resource "aws_cloudwatch_event_target" "step_function_target" {
-#   rule      = aws_cloudwatch_event_rule.scheduler.name
-#   arn       = aws_sfn_state_machine.sfn_state_machine.arn
-#   role_arn  = aws_iam_role.lambda_role.arn
-# }
-
-# Note sure if we would need the following
 # resource "aws_lambda_permission" "allow_eventbridge" {
 #   statement_id  = "AllowExecutionFromEventBridge"
 #   action        = "lambda:InvokeFunction"
